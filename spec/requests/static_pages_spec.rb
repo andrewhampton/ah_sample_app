a3bef75f09c9410213f31ff70042bae51ce1d825
Andrew Hampton <andrewwh@gmail.com>
2014-01-15 18:15:45 -0500

    Adding test and removing the | Home from the home page.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,7 +9,12 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Home")
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should not have the custom title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title(" | Home")
     end
   end
 
