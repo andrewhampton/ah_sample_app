35ee6cad86c962431ae89cb7a5c661a2df99303a
Andrew Hampton <andrewwh@gmail.com>
2014-01-16 22:51:46 -0500

    updating the paths so we have nicer URLs

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,53 +3,53 @@ require 'spec_helper'
 describe "Static pages" do
   describe "Home page" do
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content('Sample App')
     end
 
     it "should have the right title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_title("Ruby on Rails Tutorial Sample App")
     end
 
     it "should not have the custom title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).not_to have_title(" | Home")
     end
   end
 
   describe "Help page" do
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
     end
 
     it "should have the right title" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title("Help")
     end
   end
 
   describe "About page" do
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About Us')
     end
 
     it "should have the right title" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title("About")
     end
   end
 
   describe "Contact page" do
     it "should have the content 'Contact Us'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_content('Contact Us')
     end
 
     it "should have the right title" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_title("Contact")
     end
   end
