b6dbf22ee96153b82f777941b328efd47d81b2bd
Andrew Hampton <andrewwh@gmail.com>
2014-01-25 13:28:06 -0500

    adding signout functionality and testing

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
     name                  "Andrew Hampton"
-    email                 "andrew@example.com"
+    email                 "user@example.com"
     password              "foobar"
     password_confirmation "foobar"
   end
