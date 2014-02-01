481a78caf87670a528491377345f19852e0e2f4c
Andrew Hampton <andrewwh@gmail.com>
2014-01-31 20:05:03 -0500

    adding pagination for the user index page

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,15 @@
 FactoryGirl.define do
+  sequence :name do |n|
+    "Person #{n}"
+  end
+
+  sequence :email do |n|
+    "email#{n}@example.com"
+  end
+
   factory :user do
-    name                  "Andrew Hampton"
-    email                 "user@example.com"
+    name
+    email
     password              "foobar"
     password_confirmation "foobar"
   end
